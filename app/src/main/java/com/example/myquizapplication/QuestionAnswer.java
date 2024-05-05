f7f08a52f4d6614e31cf768d7108ba500af837b2
priyadharshini gorre <priyadharshinigorre04@gmail.com>
2024-05-05 20:54:49 +0530

    This is my second commit

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "what is the capital of India?",
+            "how many continents are there in the world?",
+            "which one is not the programming language?"
+    };
+
+    public static String choices[][] = {
+            {"Newdelhi","Hyderabad","chennai","Mumbai"},
+            {"5","7","6","4"},
+            {"Java","Kotlin","Notepad","Python"}
+    };
+
+    public static String correctAnswers[] = {
+            "Newdelhi",
+            "7",
+            "Notepad"
+    };
+}
